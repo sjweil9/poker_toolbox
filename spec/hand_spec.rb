48648e008ed07f9e0360f5b7be35a2d6efde25ea
Stephen Weil <stephen.weil@gmail.com>
2020-04-12 16:56:18 -0500

    Add tests for hand_rank

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -49,9 +49,49 @@ RSpec.describe PokerToolbox::Hand do
   end
 
   context '#hand_rank' do
-    it 'should detect :straight_flushes' do
+    it 'should detect straight_flushes' do
       cards = %w[6H 7H 8H 9H TH]
       expect(hand_with(cards).hand_rank).to eq(:straight_flush)
+    end
+
+    it 'should detect four-of-a-kind' do
+      cards = %w[4H 4C 4S 4D TC]
+      expect(hand_with(cards).hand_rank).to eq(:four_of_a_kind)
+    end
+
+    it 'should detect full houses' do
+      cards = %w[4H 4C 4S 3C 3D]
+      expect(hand_with(cards).hand_rank).to eq(:full_house)
+    end
+
+    it 'should detect flushes' do
+      cards = %w[4H 7H 9H JH AH]
+      expect(hand_with(cards).hand_rank).to eq(:flush)
+    end
+
+    it 'should detect straights' do
+      cards = %w[4H 5C 6S 7D 8H]
+      expect(hand_with(cards).hand_rank).to eq(:straight)
+    end
+
+    it 'should detect three-of-a-kind' do
+      cards = %w[9H 9C 9D 3H 7C]
+      expect(hand_with(cards).hand_rank).to eq(:three_of_a_kind)
+    end
+
+    it 'should two pair' do
+      cards = %w[4H 4C 7H 7D TS]
+      expect(hand_with(cards).hand_rank).to eq(:two_pair)
+    end
+
+    it 'should detect pairs' do
+      cards = %w[TC TD 3H 7S 8C]
+      expect(hand_with(cards).hand_rank).to eq(:pair)
+    end
+
+    it 'should detect high card' do
+      cards = %w[AC JD 3D 7C 6H]
+      expect(hand_with(cards).hand_rank).to eq(:high_card)
     end
   end
 end
